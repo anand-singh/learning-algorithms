7cd561d6c434f164606a9b11e2c288051d4ffbe2
Anand Singh <imanandksingh@gmail.com>
2021-05-14 00:28:02 +0000

    Update cats-core to 2.6.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   object Version {
-    lazy val cats = "2.6.0"
+    lazy val cats = "2.6.1"
   }
 
   lazy val catsCore = "org.typelevel" %% "cats-core" % Version.cats
